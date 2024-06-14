2f34c8ed8e1dcd0984c5488662f6c8ad3c8cf9b7
Nylers8 <iscindel@gmail.com>
2024-06-14 22:27:56 +0500

    test connect

--- public/js/header.js
+++ public/js/header.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const user = JSON.parse(localStorage.getItem('user'));
     const headerHTML = `
         <header>
             <div class="logo">Логотип</div>
-            <div class="site-name"><a href="index.html">Нежная ваниль</a></div>
+            <div class="site-name">Нежная ваниль</div>
             <nav>
                 <ul>
                     <li><a href="about.html">О нас</a></li>
@@ -12,13 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
                         <a href="javascript:void(0)">Каталог</a>
                         <ul class="dropdown" id="catalog-dropdown"></ul>
                     </li>
-                    ${user ? `
-                    <li><a href="javascript:void(0)">${user.first_name} ${user.last_name}</a></li>
-                    <li><a href="#" id="logout">Выйти</a></li>
-                    ` : `
                     <li><a href="login.html">Авторизация</a></li>
                     <li><a href="register.html">Регистрация</a></li>
-                    `}
                 </ul>
             </nav>
             <div class="cart">
@@ -32,20 +26,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.body.insertAdjacentHTML('afterbegin', headerHTML);
 
-    if (user) {
-        document.getElementById('logout').addEventListener('click', function() {
-            localStorage.removeItem('user');
-            fetch('http://localhost:3000/logout')
-                .then(() => {
-                    window.location.href = 'index.html';
-                })
-                .catch(error => {
-                    console.error('Ошибка при выходе:', error);
-                });
-        });
-    }
+    const baseUrl = 'https://ваш-домен.com'; // Укажите ваш хостинг-домен
 
-    fetch('http://localhost:3000/get-categories')
+    fetch(`${baseUrl}/get-categories`)
         .then(response => response.json())
         .then(data => {
             const catalogDropdown = document.getElementById('catalog-dropdown');
@@ -61,12 +44,12 @@ document.addEventListener('DOMContentLoaded', function () {
     searchInput.addEventListener('input', function () {
         const query = this.value;
         if (window.location.pathname.includes('catalog.html')) {
-            fetch(`http://localhost:3000/api/products/search?query=${query}&category=${getCategoryFromURL()}`)
+            fetch(`${baseUrl}/api/products/search?query=${query}&category=${getCategoryFromURL()}`)
                 .then(response => response.json())
                 .then(products => displayProducts(products))
                 .catch(error => console.error('Ошибка при поиске продуктов:', error));
         } else {
-            fetch(`http://localhost:3000/api/products/search?query=${query}`)
+            fetch(`${baseUrl}/api/products/search?query=${query}`)
                 .then(response => response.json())
                 .then(products => displayProducts(products))
                 .catch(error => console.error('Ошибка при поиске продуктов:', error));
@@ -87,34 +70,12 @@ function displayProducts(products) {
         const productDiv = document.createElement('div');
         productDiv.className = 'product-card';
         productDiv.innerHTML = `
-            <img src="/${product.photo_url}" alt="${product.name}" onclick="window.location.href='product.html?id=${product.product_id}'">
+            <img src="/${product.photo_url}" alt="${product.name}">
             <h3>${product.name}</h3>
             <p>${product.price} тг</p>
             <p>Количество: ${product.quantity}</p>
-            <button onclick="addToCart(${product.product_id})">Добавить в корзину</button>
+            <button>Добавить в корзину</button>
         `;
         productList.appendChild(productDiv);
     });
 }
-
-function addToCart(productId) {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (!user) {
-        alert('Пожалуйста, войдите в систему, чтобы добавить товар в корзину.');
-        return;
-    }
-
-    fetch('http://localhost:3000/api/cart', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ user_id: user.user_id, product_id: productId, quantity: 1 })
-    })
-    .then(response => response.json())
-    .then(data => {
-        alert('Товар добавлен в корзину.');
-    })
-    .catch(error => console.error('Ошибка при добавлении товара в корзину:', error));
-}
-
